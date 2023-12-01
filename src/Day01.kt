461899d9598dc970e57089621770c7c6af791420
dominikwagner <dominik.wagner@deckweiss.at>
2023-12-01 23:07:55 +0100

    day1 part1 working

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<String>): Int {
+    var sum = 0
+    input.forEach { line ->
+      var firstNumber = 0
+      var lastNumber = 0
+      line.forEach { char ->
+        if (char.isDigit()) {
+          if (firstNumber == 0) {
+            firstNumber = char.toString().toInt()
+            lastNumber = char.toString().toInt()
+          } else {
+            lastNumber = char.toString().toInt()
+          }
+        }
+        }
+       sum += firstNumber * 10 + lastNumber
+      }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    return sum
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 142)
+
+  val input = readInput("Day01")
+  part1(input).println()
+  part2(input).println()
 }
