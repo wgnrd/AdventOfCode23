eadabc1397119efb72b33b8e53c4ac5270534026
dominikwagner <dominik.wagner@deckweiss.at>
2023-12-02 10:22:46 +0100

    Refactor part1 and part2 functions in Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,56 +1,45 @@
 fun main() {
-  fun part1(input: List<String>): Int {
-    return input.sumOf { line ->
-      val digits = line.filter(Char::isDigit)
-      if (digits.isNotEmpty()) {
-        val firstNumber = digits.first().toString().toInt()
-        val lastNumber = digits.last().toString().toInt()
-        firstNumber * 10 + lastNumber
-      } else {
-        0
-      }
+  fun processLine(line: String): Int {
+    val digits = line.filter(Char::isDigit)
+    return if (digits.isNotEmpty()) {
+      val firstNumber = digits.first().toString().toInt()
+      val lastNumber = digits.last().toString().toInt()
+      firstNumber * 10 + lastNumber
+    } else {
+      0
     }
+  }
+
+  fun replaceLiteralNumbers(input: String): String {
+    val replacements = mapOf(
+      "one" to "on1e",
+      "two" to "t2wo",
+      "three" to "th3ree",
+      "four" to "fo4ur",
+      "five" to "fi5ve",
+      "six" to "si6x",
+      "seven" to "sev7en",
+      "eight" to "ei8ght",
+      "nine" to "ni9ne"
+    )
+
+    var output = input
+    replacements.forEach { (word, digit) ->
+      output = output.replace(word, digit, ignoreCase = true)
+    }
+    return output
+  }
+
+  fun part1(input: List<String>): Int {
+    return input.sumOf { line -> processLine(line) }
   }
 
   fun part2(input: List<String>): Int {
-    fun replaceLiteralNumbers(input: String): String {
-      val replacements = mapOf(
-        "one" to "on1e",
-        "two" to "t2wo",
-        "three" to "th3ree",
-        "four" to "fo4ur",
-        "five" to "fi5ve",
-        "six" to "si6x",
-        "seven" to "sev7en",
-        "eight" to "ei8ght",
-        "nine" to "ni9ne"
-      )
-
-      var output = input
-      replacements.forEach { (word, digit) ->
-        output = output.replace(word, digit, ignoreCase = true)
-      }
-      return output
-    }
-
-    return input.sumOf { line ->
-      val digits = replaceLiteralNumbers(line).filter(Char::isDigit)
-      if (digits.isNotEmpty()) {
-        val firstNumber = digits.first().toString().toInt()
-        val lastNumber = digits.last().toString().toInt()
-        firstNumber * 10 + lastNumber
-      } else {
-        0
-      }
-    }
+    return input.sumOf { line -> processLine(replaceLiteralNumbers(line)) }
   }
 
-
-
-
-
   val testInput = readInput("Day01_test")
-//  check(part1(testInput) == 142)
+  //  check(part1(testInput) == 142)
   check(part2(testInput) == 281)
 
   val input = readInput("Day01")
