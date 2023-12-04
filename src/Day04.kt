21c3ebf5c02f8722d5278dc36f7b91beae8c4183
dominikwagner <dominik.wagner@deckweiss.at>
2023-12-04 15:44:37 +0100

    day3 part2 refactor

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,42 +1,37 @@
 fun main() {
+  fun parseCardData(line: String): Pair<List<Int>, List<Int>> {
+    val cardData = line.substringAfter(": ")
+    val winningNumbers = cardData.substringBefore(" | ")
+      .split(" ")
+      .filter { it.isNotEmpty() }
+      .map { it.toInt() }
+    val cardNumbers = cardData.substringAfter(" | ")
+      .split(" ")
+      .filter { it.isNotEmpty() }
+      .map { it.toInt() }
+    return winningNumbers to cardNumbers
+  }
+
   fun part1(input: List<String>): Int {
     return input.sumOf { line ->
-      val cardData = line.substringAfter(": ")
-      val winningNumbers =
-        cardData.substringBefore(" | ").split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
-      val cardNumbers =
-        cardData.substringAfter(" | ").split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
-
-      cardNumbers
-        .fold(0) { cardValue, number ->
-          if (number in winningNumbers) {
-            if (cardValue == 0) cardValue + 1 else cardValue * 2
-          } else {
-            cardValue
-          }
+      val (winningNumbers, cardNumbers) = parseCardData(line)
+      cardNumbers.fold(0) { cardValue, number ->
+        if (number in winningNumbers) {
+          if (cardValue == 0) cardValue + 1 else cardValue * 2
+        } else {
+          cardValue
         }
-        .toInt()
+      }.toInt()
     }
   }
 
   fun part2(input: List<String>): Int {
     val scratchCardsInstances = input.indices.associateWith { 1 }.toMutableMap()
-    var amountOfWinningNumbers = 0
     input.forEachIndexed { idx, line ->
-      amountOfWinningNumbers = 0
-      val cardData = line.substringAfter(": ")
-      val winningNumbers =
-        cardData.substringBefore(" | ").split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
-      val cardNumbers =
-        cardData.substringAfter(" | ").split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
-
-      cardNumbers.forEach { number ->
-        if (number in winningNumbers) {
-          amountOfWinningNumbers++
-        }
-      }
-      for (i in idx until idx +amountOfWinningNumbers) {
-        scratchCardsInstances[i+1] = scratchCardsInstances[i+1]!! + scratchCardsInstances[idx]!!
+      val (winningNumbers, cardNumbers) = parseCardData(line)
+      val amountOfWinningNumbers = cardNumbers.count { it in winningNumbers }
+      for (i in idx until idx + amountOfWinningNumbers) {
+        scratchCardsInstances[i + 1] = scratchCardsInstances[i + 1]!! + scratchCardsInstances[idx]!!
       }
     }
     return scratchCardsInstances.values.sum()
